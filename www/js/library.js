4c687cfeac92d161d9106a22f60ffcaaf8e78d24
David Olivari <david.olivari@gmail.com>
2015-04-01 15:37:22 +0200

    moving basic function to library file adding 5 buttons adding udpate to setState function

---
+++ www/js/library.js
@@ -0,0 +1,35 @@
+var darkGrey = new Color(.3);
+
+function triangle(start) {
+    var path = new Path();
+    path.fillColor = 'black';
+    path.strokeColor = 'grey';
+    path.moveTo(start);
+    path.lineTo(start + [100, 0]);
+    path.lineTo(start + [50, 150]);
+    path.lineTo(start);
+    path.activeFill = {
+        gradient: {
+            stops: ['black', darkGrey]
+        },
+        origin: start + [50, 0],
+        destination: start + [50, 150]
+    };
+    path.setState = function(state) {
+        this.fillColor = (state ? this.activeFill : "black");
+        paper.view.update();
+    }
+    return path;
+}
+
+red = new Array();
+
+for (var l = 9; 0 <= l; l--) {
+    red[l] = new Array();
+    for (var c = 0; c < 10; c++) {
+        var s1 = new Point(c * 100, l * 100);
+        var s2 = new Point((c * 100) - 50, (l * 100) - 50);
+        red[l][c * 2] = triangle(s1);
+        red[l][1 + c * 2] = triangle(s2);
+    }
+}
--- www/js/main.js
+++ www/js/main.js
@@ -1,40 +1,26 @@
-var darkGrey = new Color(.3);
+$(function() {
 
-function triangle(start) {
-    var path = new Path();
-    path.fillColor = 'black';
-    path.strokeColor = 'grey';
-    path.moveTo(start);
-    path.lineTo(start + [100, 0]);
-    path.lineTo(start + [50, 150]);
-    path.lineTo(start);
-    path.activeFill = {
-        gradient: {
-            stops: ['black',darkGrey ]
-        },
-        origin: start +[50,0],
-        destination: start +[50,150]
-    };
-    return path;
-}
+    $("#prog1").click(function() {
+        red[6][7].setState(true);
+    });
 
-red = new Array();
+    $("#prog2").click(function() {
+        red[6][7].setState(false);
+    });
 
-for (var l = 9; 0 <= l; l--) {
-    red[l] = new Array();
-    for (var c = 0; c < 10; c++) {
-        var s1 = new Point(c * 100, l * 100);
-        var s2 = new Point((c * 100) - 50, (l * 100) - 50);
-        red[l][c * 2] = triangle(s1);
-        red[l][1 + c * 2] = triangle(s2);
-    }
-}
+    $("#prog3").click(function() {
 
 
+    });
 
-function onFrame(event) {
-    if (0 == event.count % 1) {
-        var tr = red[Math.floor(Math.random() * 9)][1+Math.floor(Math.random() * 19)];
-        tr.fillColor = (.5 < Math.random() ? tr.activeFill : "black");
-    }
-}
+    $("#prog4").click(function() {
+
+
+    });
+
+    $("#prog5").click(function() {
+
+
+    });
+
+});

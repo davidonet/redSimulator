e15d287f26713c3681630349cff48e3011cab5c2
David Olivari <david.olivari@gmail.com>
2015-04-02 10:40:09 +0200

    random loop test

--- www/js/main.js
+++ www/js/main.js
@@ -1,26 +1,31 @@
 $(function() {
 
     $("#prog1").click(function() {
-        red[6][7].setState(true);
+        red[Math.floor(Math.random() * 10)][Math.floor(Math.random() * 20)].setState(true);
     });
 
     $("#prog2").click(function() {
-        red[6][7].setState(false);
+        red[Math.floor(Math.random() * 10)][Math.floor(Math.random() * 20)].setState(false);
     });
 
+    var loop = false;
+    var looping = function() {
+        if (loop) {
+            red[Math.floor(Math.random() * 10)][Math.floor(Math.random() * 20)].setState(Math.random() < .5);
+            setTimeout(looping, 100)
+        }
+    }
     $("#prog3").click(function() {
-
-
+        loop = true;
+        looping();
     });
 
     $("#prog4").click(function() {
-
-
+        loop = false;
     });
 
     $("#prog5").click(function() {
-
-
+        cleanRed();
     });
 
 });
